507b328141fe587cb624867352119a897c2dcb5f
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-03-16 21:57:20 +0100

    Create h1 element that displays from array my name and title from a function getTitle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+// const welcome = {
+//   greeting: 'Hey',
+//   title: 'React',
+// };
+
+function getTitle(title) {
+  return title;
+}
+const myArray = ['My', 'name', 'is', 'Marek'];
+const getName = myArray.map((el) => {
+  return el;
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>{welcome.greeting} {welcome.title}</h1> */}
+      <h1>{getName} and this is {getTitle('React')} </h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' placeholder='write something' />
+      <h2>
+        I want to learn React so i can get a good job, and my parents can be
+        proud of me and i will no longer be a loser and a burden for them!
+      </h2>
     </div>
   );
 }
