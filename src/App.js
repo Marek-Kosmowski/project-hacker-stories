de13162a3d996768d65b9f551876aa564e2896e5
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-03-17 18:43:16 +0100

    Create a list array, and display it as a list on a page

--- src/App.js
+++ src/App.js
@@ -13,18 +13,49 @@ const getName = myArray.map((el) => {
   return el;
 });
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.js.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function App() {
   return (
     <div>
       {/* <h1>{welcome.greeting} {welcome.title}</h1> */}
-      <h1>{getName} and this is {getTitle('React')} </h1>
+      <h1>
+        {getName} and this is {getTitle('React')}{' '}
+      </h1>
 
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' placeholder='write something' />
-      <h2>
-        I want to learn React so i can get a good job, and my parents can be
-        proud of me and i will no longer be a loser and a burden for them!
-      </h2>
+      <ul>
+        {list.map((item) => {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title} </a>
+              </span>
+              <span>{item.author} </span>
+              <span>{item.num_comments} </span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
